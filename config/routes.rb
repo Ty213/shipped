e87b5fb4b214efbc37d5a1a0149f01bc89c1bfb3
Ty Thompson <tylermoko@gmail.com>
2018-03-01 16:18:26 -0500

    adding controllers/navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,31 @@
 Rails.application.routes.draw do
 
+  get 'jobs/create'
+
+  get 'jobs/new'
+
+  get 'jobs/index'
+
+  get 'jobs/show'
+
+  get 'jobs/edit'
+
+  get 'jobs/update'
+
+  get 'jobs/destroy'
+
+  get 'boats/index'
+
+  get 'boats/create'
+
+  get 'boats/new'
+
+  get 'boats/show'
+
+  get 'boats/update'
+
+  get 'boats/destroy'
+
   devise_for :users
 
 	root "welcome#index"
