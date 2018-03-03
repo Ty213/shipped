fcdc7cccf7bab552ba0328086cc35ce6a17c1282
Ty Thompson <tylermoko@gmail.com>
2018-03-03 15:23:59 -0500

    adding description length validation

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,7 @@
 class Job < ApplicationRecord
 	validates :name, presence: true, uniqueness: true
 	validates_numericality_of :budget, greater_than: 100
+	validates :description, length: { minimum: 20 }
 	belongs_to :user
 	has_many :boats
 end
