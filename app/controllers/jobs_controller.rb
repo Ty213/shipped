f7a821a4666f609498c09e53b7ef1f1f49f53298
aciukurescu <andrewciukurescu@gmail.com>
2018-03-05 14:00:33 -0500

    boats and jobs are dependent on user

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,7 @@ class JobsController < ApplicationController
   def create
     @job = Job.new(job_params)
 		if @job.save
-			redirect_to root_path
+			redirect_to "/jobs"
 		else
 			render new_job_path
 		end
