0166f3f3d968b4f3fb626cd88f053db9786b13fd
Lee Barber <lfb11@alumni.psu.edu>
2018-03-05 11:10:30 -0500

    added edit and delete functionality to user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,3 +3,16 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         end
 end
+
+ def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+    redirect_to @user
+  end
+  end
+
+ def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to "/boats"
+  end
